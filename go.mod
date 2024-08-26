1147d608da9ee9a80554cf919d07cc2244bd0405
Amit Dubey <amitkd2018@gmail.com>
2024-08-26 18:26:36 +0530

    raw completed

--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/dubey2k/inmem-db
-
-go 1.22.1
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net"
+	"strings"
 )
 
 func main() {
@@ -14,6 +15,26 @@ func main() {
 		fmt.Println(err)
 		return
 	}
+
+	aof, err := NewAof("database.aof")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer aof.Close()
+
+	aof.Read(func(value Value) {
+		command := strings.ToUpper(value.array[0].bulk)
+		args := value.array[1:]
+
+		handler, ok := Handlers[command]
+		if !ok {
+			fmt.Println("Invalid command: ", command)
+			return
+		}
+
+		handler(args)
+	})
 
 	// Listen for connections
 	conn, err := l.Accept()
@@ -32,9 +53,33 @@ func main() {
 			return
 		}
 
-		fmt.Println(value)
+		if value.typ != "array" {
+			fmt.Println("Invalid request, expected array")
+			continue
+		}
 
-		// ignore request and send back a PONG
-		conn.Write([]byte("+OK\r\n"))
+		if len(value.array) == 0 {
+			fmt.Println("Invalid request, expected array length > 0")
+			continue
+		}
+
+		command := strings.ToUpper(value.array[0].bulk)
+		args := value.array[1:]
+
+		writer := NewWriter(conn)
+
+		handler, ok := Handlers[command]
+		if !ok {
+			fmt.Println("Invalid command: ", command)
+			writer.Write(Value{typ: "string", str: ""})
+			continue
+		}
+
+		if command == "SET" || command == "HSET" {
+			aof.Write(value)
+		}
+
+		result := handler(args)
+		writer.Write(result)
 	}
 }
